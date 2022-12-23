56ef06295d381a6cad45f23d4710a2fa57612909
florent2206 <113612660+florent2206@users.noreply.github.com>
2022-12-23 12:27:55 +0100

    Delete accueil.php

--- accueil.php
+++
@@ -1,6 +0,0 @@
-<?php
-    // ################## Page Accueil ##################################
-    $data=yaml_parse_file("accueil.yaml");
-    echo '<p id="Nom">'.$data['nom'].' '.$data['prenom'].'</p>';
-    echo '<p id="Ent">'.$data['accroche'].'</p>'; 
-?>
